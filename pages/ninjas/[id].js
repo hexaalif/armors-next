9aef54fcce3d42d838d9963c2be8f0b9af2b371e
hexaalif <hexaalif2020@gmail.com>
2023-02-25 17:57:47 +0600

    paths added

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,20 @@
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  // paths: [{}, {}, {params: {id}}
+  const paths = data.map((arm) => {
+    return {
+      params: { id: arm.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
